8ed577511b3e41894611926e4b79846d73e6700d
Brianx03 <brianx03@gmail.com>
2023-05-03 19:12:05 -0600

    Removed newtonsoft json nuget

--- Services/UsersService.cs
+++ Services/UsersService.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using UsersManager.Interfaces;
+﻿using UsersManager.Interfaces;
 using UsersManager.Models;
 
 namespace UsersManager.Services;
@@ -7,39 +6,13 @@ namespace UsersManager.Services;
 public class UsersService : IUsersService
 {
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly string url = "https://jsonplaceholder.typicode.com/users";
 
-    public UsersService(IHttpClientFactory httpClientFactory)
-    {
-        _httpClientFactory = httpClientFactory;
-    }
+    public UsersService(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
 
-    public async Task<User> GetUserDataAsync(int userId)
-    {
-        var client = _httpClientFactory.CreateClient();
-        var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/users/{userId}");
+    public async Task<User> GetUserDataAsync(int userId) =>
+        await _httpClientFactory.CreateClient().GetFromJsonAsync<User>($"{url}/{userId}");
 
-        if (response.IsSuccessStatusCode)
-        {
-            var json = await response.Content.ReadAsStringAsync();
-            var user = JsonConvert.DeserializeObject<User>(json);
-            return user;
-        }
-
-        return null;
-    }
-
-    public async Task<List<User>> GetAllUsersDataAsync()
-    {
-        var client = _httpClientFactory.CreateClient();
-        var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/users");
-
-        if (response.IsSuccessStatusCode)
-        {
-            var json = await response.Content.ReadAsStringAsync();
-            var users = JsonConvert.DeserializeObject<List<User>>(json);
-            return users;
-        }
-
-        return null;
-    }
+    public async Task<List<User>> GetAllUsersDataAsync() =>
+        await _httpClientFactory.CreateClient().GetFromJsonAsync<List<User>>(url);
 }
